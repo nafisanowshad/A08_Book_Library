30d1188fe08532f91cffdd9b25e698e1bd1199f7
nafisanowshad <nafisanowshadmithila@gmail.com>
2026-08-14 00:47:21 +0600

    category added

--- src/app/all-books/page.jsx
+++ src/app/all-books/page.jsx
@@ -1,15 +1,24 @@
 import BookCard from "@/components/BookCard";
+import Category from "@/components/Category";
 
-const AllBooksPage = async() => {
+const AllBooksPage = async({searchParams}) => {
+    const {category}=await searchParams;
+    // console.log(category);
+
     const res=await fetch('https://a08-book-library.vercel.app/data.json')
     const books=await res.json()
     // console.log(books)
+
+    const filteredBooks= category ? books.filter(book=>book.category.toLowerCase() == category.toLowerCase()) : books
+
     return (
         <div className="mb-15">
             <h1 className="text-2xl font-bold mx-8 my-6">All Books</h1>
+
+            <Category />
             <div className="grid grid-cols-4 gap-5 px-6">
                 {
-                    books.map(book=> <BookCard key={book.id} book={book} />)
+                    filteredBooks.map(book=> <BookCard key={book.id} book={book} />)
                 }
             </div>
         </div>
