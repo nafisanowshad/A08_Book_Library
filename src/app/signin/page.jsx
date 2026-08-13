59bfdab1624a2a3db93ca81c1900c0e9fbd55540
nafisanowshad <nafisanowshadmithila@gmail.com>
2026-08-13 21:36:50 +0600

    signin authentication added

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -11,52 +11,30 @@ import {
   Label,
   TextField,
 } from "@heroui/react";
-import { useRouter } from "next/navigation";
 
-export default function SignUpPage() {
-
-  const router= useRouter()
-
+export default function SignInPage() {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    const name= e.target.name.value;
-    const image= e.target.image.value;
     const email= e.target.email.value;
     const password= e.target.password.value;
 
     // console.log({name, image, email, password})
 
-    const {data, error}= await authClient.signUp.email({
-        name,
+    const {data, error}= await authClient.signIn.email({
         email,
         password,
-        image,
+        callbackURL: '/'
     })
 
     // console.log({data, error});
-
-    if(!error){
-      router.push('/')
-    }
   };
 
   return (
     <Card className="border mx-auto w-125 py-10 mt-5">
-      <h1 className="text-center text-2xl font-bold">Sign Up</h1>
+      <h1 className="text-center text-2xl font-bold">Sign In</h1>
 
       <Form className="flex w-96 mx-auto flex-col gap-4" onSubmit={onSubmit}>
-        <TextField isRequired name="name" type="text">
-          <Label>Name</Label>
-          <Input placeholder="Enter your name" />
-          <FieldError />
-        </TextField>
-
-        <TextField isRequired name="image" type="text">
-          <Label>Image URL</Label>
-          <Input placeholder="Image URL" />
-          <FieldError />
-        </TextField>
 
         <TextField
           isRequired
